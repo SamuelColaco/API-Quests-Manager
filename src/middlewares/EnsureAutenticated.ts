d6d10d0b986aff2f11e4f61fab1cf0bc362d18d2
Samuel <colaco.samuel8@gmail.com>
2025-01-27 07:20:34 -0300

    Criando o tipo user do request

--- src/middlewares/EnsureAutenticated.ts
+++ src/middlewares/EnsureAutenticated.ts
@@ -1,6 +1,9 @@
 
-import {Request, Response, NextFunction} from "express"
+import { Request, Response, NextFunction } from "express"
 import { AppError } from "../utils/AppError"
+import { verify } from "jsonwebtoken"
+import { authConfig } from "../config/auth"
+
 
 export function EnsureAutenticated(req: Request, res: Response, next: NextFunction){
     
@@ -9,6 +12,14 @@ export function EnsureAutenticated(req: Request, res: Response, next: NextFuncti
     if(!authHeader){
         throw new AppError("Não está autenticado")
     }
-    
+
+    const authHeaderToken = authHeader.slice(7)
+
+     const { sub: userId } = verify(authHeaderToken, authConfig.jwt.secret)
+
+     req.user = {
+        id: String(userId)
+     }
+
     return next()
 }
--- src/routes/teams.ts
+++ src/routes/teams.ts
@@ -1,12 +1,13 @@
 
 import { Router } from "express"
 import { TeamControllers } from "../controllers/TeamsControllers"
+import { EnsureAutenticated } from "../middlewares/EnsureAutenticated"
 
 const teamsRoutes = Router()
 const teamController = new TeamControllers()
 
 teamsRoutes.get("/teams", teamController.index)
-teamsRoutes.post("/teams", teamController.create)
+teamsRoutes.post("/teams", EnsureAutenticated, teamController.create)
 teamsRoutes.put("/teams/:id", teamController.update)
 
 export { teamsRoutes }
